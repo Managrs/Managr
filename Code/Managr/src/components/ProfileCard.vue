76d706a5539b9f9598468f3f7ebfa8a05fbde84d
dimpho <mateadimpho91@gmail.com>
2025-04-14 10:38:59 +0200

    fix azure deployment

--- Code/Managr/src/components/ProfileCard.vue
+++ Code/Managr/src/components/ProfileCard.vue
@@ -12,7 +12,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, type PropType } from 'vue';
 
 export default defineComponent({
   name: 'ProfileCard',
