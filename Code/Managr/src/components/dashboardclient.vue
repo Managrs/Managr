8207591c668275a8ccf86b26320fb9c5f19e75df
LemohangMotsapi <2655576@students.wits.ac.za>
2025-04-18 18:20:16 +0200

    Random editing

--- Code/Managr/src/components/dashboardclient.vue
+++ Code/Managr/src/components/dashboardclient.vue
@@ -6,7 +6,8 @@
       </section>
       <nav>
         <button class="post-btn">Post Project</button>
-        <button class="messages-btn">Messages</button>
+        <button id="messages-btn">Messages</button>
+        <!-- <button onclick ="location.href='https://wa.me/+27814903174/?text=Conntect me on whatsapp'">Message</button>-->
         <section class="profile-picture">
           <img :src="profilePic" alt="Profile Picture" />
         </section>
@@ -47,6 +48,11 @@ import logo from '../assets/logo1.jpg'
 import profilePic from '../assets/noprofile.jpg'
 
 console.log("Dashboard ready");
+
+document.getElementById("message-btn").onclick = function(){
+
+}
+
 </script>
 
 <style scoped>
