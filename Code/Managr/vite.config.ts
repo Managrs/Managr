5a341c0821af0182b2746091bf5dc7ad95899cc9
Dimpho Matea <125918769+CYBER-SKUVA@users.noreply.github.com>
2025-04-20 20:42:52 +0200

    configuring the build

--- Code/Managr/vite.config.ts
+++ Code/Managr/vite.config.ts
@@ -6,7 +6,15 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     headers: {
-      'Content-Type': 'application/javascript' // Ensures correct JS/TS MIME types
+      'Content-Type': 'application/javascript' // Forces correct MIME type
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: '[name].[hash].js', // Clean chunk naming
+        assetFileNames: '[name].[hash][extname]'
+      }
     }
   }
 })
