036d11f03a411149db7a0d76965ea474af1739cf
LemohangMotsapi <2655576@students.wits.ac.za>
2025-04-21 12:50:25 +0200

    Overwrote LandingPagE

--- Code/Managr/src/components/LandingPage.vue
+++ Code/Managr/src/components/LandingPage.vue
@@ -1,7 +1,4 @@
 <template>
-  <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-  <script src="https://unpkg.com/@vue/compiler-sfc@3/dist/compiler-sfc.js"></script>
-    
   <header class="header">
     <nav class="navigation">
       <router-link to="/" class="logo-link" aria-label="Home">
@@ -11,12 +8,11 @@
       <ul class="nav-links">
         <li>
           <!--<router-link to="/dashboard" class="nav-link">Login</router-link>-->
-          <button class="nav-link" @click="handleLogin">Sign in</button>
+          <button class="nav-link" @click="handleLogin">Log in</button>
         </li>
-
         <li>
           <!--<router-link to="/dashboard" class="nav-link">Signup</router-link>-->
-          <!--<button class="nav-link" @click="handleLogout" >Log out</button>-->
+          <button class="nav-link" @click="handleLogout" >Log out</button>
         </li>
       </ul>
     </nav>
@@ -28,7 +24,7 @@
   <section class="overlay-content">
     <h1 class="main-title">Welcome to Managr</h1>
     <p class="main-subtitle">Your platform for freelancers and clients to connect seamlessly.</p>
-    <router-link to="/dashboardadmin" class="cta-button">Get Started with Managr</router-link>
+    <router-link to="/dashboardadmin" class="cta-button">Get Started</router-link>
   </section>
 </section>
     </section>    
@@ -50,18 +46,17 @@ import { RouterLink } from 'vue-router';
 
 const { loginWithRedirect } = useAuth0();
 
-/*const { logout } = useAuth0();*/
+const { logout } = useAuth0();
 
 const handleLogin = () => {
   loginWithRedirect();
 };
 
-
-/*const handleLogout = () => {
+const handleLogout = () => {
   logout({  
     logoutParams:  {returnTo: window.location.origin}
    });
-};*/
+};
 
 </script>
   
