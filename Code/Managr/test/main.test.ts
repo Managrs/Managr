6b4e714ea4210aff6de41a22dc8745b6451a3e2c
Lebo-Kharafu <Kharafu23@gmail.com>
2025-04-23 20:13:58 +0200

    Lesique the tester adaded testing vue

--- Code/Managr/test/main.test.ts
+++ Code/Managr/test/main.test.ts
@@ -1,7 +1,12 @@
 import { it,expect,describe} from "vitest";
 
+//passing test to setup
 describe('passer', () => {
     it('Should',() => {
         expect(1).toBeTruthy();
     });
 });
+
+//Check that imports are correct
+
+//Check app uses are added
