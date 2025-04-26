d4465cb5810341195127747bf4be893d709c8710
Dimpho Matea <125918769+CYBER-SKUVA@users.noreply.github.com>
2025-04-26 18:41:54 +0200

    fixing server

--- Code/managrs-server/index.js
+++ Code/managrs-server/index.js
@@ -65,7 +65,7 @@ app.get('/getGig', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
