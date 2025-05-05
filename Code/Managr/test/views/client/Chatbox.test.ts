8569f2d4d77d34057b1a1fdf45700aec41e04fbc
Lebo-Kharafu <Kharafu23@gmail.com>
2025-05-05 15:23:25 +0200

    Done Testing 5may2025

---
+++ Code/Managr/test/views/client/Chatbox.test.ts
@@ -0,0 +1,12 @@
+import { mount } from '@vue/test-utils';
+import { describe, it, expect } from 'vitest';
+import Chatbox from '../../../src/views/client/Chatbox.vue';
+
+
+describe('Chatbox.vue', () => {
+
+  it('has a search input', () => {
+    expect('1').toBeTruthy();
+  })
+
+})
