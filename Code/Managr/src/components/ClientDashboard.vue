099762123f00c71d54efe2c35ea40a629da26419
LemohangMotsapi <2655576@students.wits.ac.za>
2025-05-23 23:04:57 +0200

    Random

--- Code/Managr/src/components/ClientDashboard.vue
+++ Code/Managr/src/components/ClientDashboard.vue
@@ -6,6 +6,7 @@
     <nav>
       <button class="post-btn">Post Project</button>
       <button class="messages-btn">Messages</button>
+      <button class=""></button>
       <section class="profile-picture">
         <img src="/noprofile.png" alt="Profile Picture" />
       </section>
