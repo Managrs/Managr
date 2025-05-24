b3569cc682c63c032292323b5edde7b021d2bb5f
Lebo-Kharafu <Kharafu23@gmail.com>
2025-05-24 12:36:50 +0200

    Added tarcking files to tests

---
+++ Code/Managr/test/views/tracking/clientTracking.test.ts
@@ -0,0 +1,4 @@
+import { mount } from '@vue/test-utils';
+import { describe, it, expect } from 'vitest';
+import clientTrack from '../../../src/views/tracking/clientTracking.vue';
+
---
+++ Code/Managr/test/views/tracking/freelanceTracking.test.ts
@@ -0,0 +1,4 @@
+import { mount } from '@vue/test-utils';
+import { describe, it, expect } from 'vitest';
+import freelanceTrack from '../../../src/views/tracking/freelanceTracking.vue';
+
